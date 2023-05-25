da63cb75429c548e289abc6f4675e7e639f66a88
Marvin Argollo <111886854+ArgolloTechnology@users.noreply.github.com>
2023-05-25 18:06:29 +0100

    new index

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+
+ const body = document.querySelector("body"),
+    modeToggle = body.querySelector(".mode-toggle");
+    sidebar = body.querySelector("nav");
+    sidebarToggle = body.querySelector(".sidebar-toggle");
+
+
+    modeToggle.addEventListener("click", () => {
+        body.classList.toggle("dark");
+    });
+
+    sidebarToggle.addEventListener("click", () => {
+        sidebar.classList.toggle("close");
+    })
+   
